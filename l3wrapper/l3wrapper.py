a4446df95ab647c31207ae22c6cc084fde99181d
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2020-03-03 16:42:51 +0100

    Add score function.

--- l3wrapper/l3wrapper.py
+++ l3wrapper/l3wrapper.py
@@ -224,4 +224,21 @@ class L3Classifier:
         return np.array(y_pred)
 
     def score(self, X, y):
-        pass
+        """
+        Return the mean accuracy on the given test data and labels.
+        In multi-label classification, this is the subset accuracy
+        which is a harsh metric since you require for each sample that
+        each label set be correctly predicted.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+            True labels for X.
+        Returns
+        -------
+        score : float
+            Mean accuracy of self.predict(X) wrt. y.
+        """
+        from sklearn.metrics import accuracy_score
+        return accuracy_score(y, self.predict(X))
